5b3c076bdfb322cfb92f7a023f2055c900819380
Bruno Frigeri <bpiraja97@gmail.com>
2018-11-09 13:00:59 -0200

    Other test

--- server.js
+++ server.js
@@ -7,20 +7,26 @@ const app = express();
 const { Todo } = require('./src/models/todo');
 const { User } = require('./src/models/user');
 
-app.use('/todos', require('./src/routes'));
+app.use('/', require('./src/routes'));
 app.use(express.json());
 
-app.post('/todos', (req, res) => {
-  var todo = new Todo ({
-    text: req.body.text
-  });
+ app.post('/todos', (req, res) => {
+   var todo = new Todo ({
+     text: req.body.text
+   });
+   todo.save().then((doc) => {
+     res.send(doc);
+   }, (e) => {
+     res.status(400).send(e);
+   });
+ });
 
-  todo.save().then((doc) => {
-    res.send(doc);
+ app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  });
-
+  })
 });
 
 app.listen(3000);
